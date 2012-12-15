4ba9968dfaf803e300b9f104f456b9bf42a5f909
Kyle Kellogg <kyle@kylekellogg.com>
2012-12-15 09:55:26 -0500

    Added skeletal function for checking for parking, added comments

--- main.lua
+++ main.lua
@@ -17,16 +17,24 @@ end
 
 function the.app.moveY( object, positive )
   if positive then
-    
+    --  Move object one tile up
   else
-    
+    --  Move object one tile down
   end
 end
 
 function the.app.moveX( object, positive )
   if positive then
-    
+    --  Move object one tile right
   else
-    
+    --  Move object one tile left
+  end
+end
+
+function the.app.checkForParking( object, positive )
+  if positive then
+    --  Check for parking one tile up, one tile to the right
+  else
+    --  Check for parking one tile down, one tile to the left
   end
 end
