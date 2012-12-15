eb8f9f0a3faa5359685ed3dca41bfdf9d771c674
Kyle Kellogg <kyle@kylekellogg.com>
2012-12-15 10:31:43 -0500

    Fixed merge issues, added thens, added separators between classes

--- main.lua
+++ main.lua
@@ -23,50 +23,57 @@ function the.app:onEndFrame()
   --  Check for input, flag as moving
 end
 
+---------------------------------------------------------------------------------------------------------
+
 MovingTile = Tile:extend
 {
 }
 
 function MovingTile:move( dir )
-  if dir == UP
+  if dir == UP then
     --  Move one tile up
-  elseif dir == DOWN
+  elseif dir == DOWN then
     --  Move one tile down
-  elseif dir == LEFT
+  elseif dir == LEFT then
     --  Move one tile left
-  elseif dir == RIGHT
+  elseif dir == RIGHT then
     --  Move one tile right
   end
 end
 
 function MovingTile:checkIfTileIsOpen( dir )
-  if dir == UP
+  if dir == UP then
     --  Check if tile above is open
-  elseif dir == DOWN
+  elseif dir == DOWN then
     --  Check if tile below is open
-  elseif dir == LEFT
+  elseif dir == LEFT then
     --  Check if tile to the left is open
-  elseif dir == RIGHT
+  elseif dir == RIGHT then
     --  CHeck if tile to the right is open
   end
 end
+
+---------------------------------------------------------------------------------------------------------
 
 Car = MovingTile:extend
 { 
 }
 
 function Car:checkForParking( dir )
-  if dir == UP
+  if dir == UP then
     --  Check for parking one tile up, one tile to the right
-  elseif dir == DOWN
+  elseif dir == DOWN then
     --  Check for parking one tile down, one tile to the left
   end
 end
 
+---------------------------------------------------------------------------------------------------------
+
 Player = MovingTile:extend
 {
 }
-end
+
+---------------------------------------------------------------------------------------------------------
 
 MapView = View:extend
 {
