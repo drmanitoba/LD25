ade02e4d2576d6b34035794397b9b77f0ffe13a5
Dan Barron <dan@minibigdog.com>
2012-12-15 10:18:30 -0500

    add dummy map'

--- main.lua
+++ main.lua
@@ -5,11 +5,13 @@ require 'zoetrope'
 the.app = App:new
 {
 	onRun = function (self)
-		self:add(Fill:new{ width = 16, height = 16,
-			x = (self.width - 16) / 2,
-			y = (self.width - 16) / 2,
-			fill = {0, 0, 255},
-			velocity = { rotation = math.pi / 2 }
-		})
+    self.view = MapView:new()
 	end
+}
+
+MapView = View:extend{
+  onNew = function(self)
+    self:loadLayers("res/map.lua")
+    self:clampTo(self.map)
+  end
 }
