e38699593f56013f8c6b068815f0dea58fce2bf8
Dan Barron <dan@minibigdog.com>
2012-12-17 17:29:59 -0500

    finished title screen

--- StartView.lua
+++ StartView.lua
@@ -1,12 +1,36 @@
+StartText = Text:extend
+{
+  font = { "res/Minecraftia.ttf", 24 },
+  width = 350,
+  height = 40
+}
+
 StartView = View:extend
 {
   startTile = Tile:new{
     x = 0,
     y = 0,
     image = "res/super_meter_maid_title.png"
+  },
+  startGameText = StartText:new{
+    x = 430,
+    y = 600,
+    text = "Press 'z' to start"
+  },
+  creditsText = StartText:new{
+    x = 430,
+    y = 400,
+    font = { "res/Minecraftia.ttf", 18 },
+    width = 400,
+    text = "Art by: Mike Schondek \n" ..
+    "Programming: Kyle Kellogg \n" ..
+    "& Dan Barron \n" ..
+    "Music by: Dwight Edwards"
   }
 }
 
 function StartView:onNew()
   self:add(self.startTile)
+  self:add(self.startGameText)
+  self:add(self.creditsText)
 end
